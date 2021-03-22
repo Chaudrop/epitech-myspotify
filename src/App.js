67a726181f87fb76d5bb392f04428c73a55b533a
Pierre <pierre.chaudron@epitech.eu>
2021-03-22 19:22:56 +0100

    feat(auth): login page design

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import React from 'react';
 import { SpotifyAuth, Scopes } from 'react-spotify-auth';
 import 'react-spotify-auth/dist/index.css';
 
+import { Container, Typography } from '@material-ui/core';
+
 import './App.css';
 
 const App = () => {
@@ -13,12 +15,17 @@ const App = () => {
         <p>You are authorized with token: {token}</p>
       ) : (
         // Display the login page
-        <SpotifyAuth
-          redirectUri="http://localhost:3000/callback"
-          clientID="1a70ba777fec4ffd9633c0c418bdcf39"
-          scopes={[Scopes.userReadPrivate, 'user-read-email']} // either style will work
-          btnClassName="test"
-        />
+        <Container maxWidth="md" className="login-page-container">
+          <Typography noWrap className="my-spotify-title" variant="h1" align="center">
+            MySpotify
+          </Typography>
+          <SpotifyAuth
+            redirectUri="http://localhost:3000/callback"
+            clientID="1a70ba777fec4ffd9633c0c418bdcf39"
+            scopes={[Scopes.userReadPrivate, 'user-read-email']}
+            btnClassName="login-button"
+          />
+        </Container>
       )}
     </div>
   );
